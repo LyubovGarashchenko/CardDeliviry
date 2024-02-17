902416d8d3135c37b097cd4cca04232d6052e137
Lyubov Garashchenko <lyubov-balabina@yandex.ru>
2024-02-17 15:33:04 +0300

    Finish commit

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,8 +1,13 @@
-package  ru.netology;
+package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -11,12 +16,13 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CardDeliveryTest {
+
     private String generateDate(int addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Test
-    public void shouldTestSuccessRegistrationForm () {
+    public void shouldTestSuccessRegistrationForm() {
         Selenide.open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Петропавловск-Камчатский");
         String planningDate = generateDate(3, "dd.MM.yyyy");
